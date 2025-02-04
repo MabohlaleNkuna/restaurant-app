c37a62ac1417b2dcd6bc9aa82a302460a2086080
Yolanda-landii <landiimabotja2000@gmail.com>
2025-02-04 08:50:58 +0200

    connected to the server

--- utils/Api.js
+++ utils/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const api = axios.create({
-  baseURL: 'http://192.168.92.198:5000/api',
+  baseURL: 'http://restaurantapp-server-1.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
